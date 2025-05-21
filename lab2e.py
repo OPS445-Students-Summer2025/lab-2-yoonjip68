704859c48495b348418b2af2515e87fcce9c4def
Yoonji Park <ypark68@myseneca.ca>
2025-05-20 21:35:45 -0400

    lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Yoonji Park
+# Author ID: ypark68
+# Date Created: 2025/05/20
+
+count = 10
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("blast off!")
+
