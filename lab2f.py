3bef55b1145cd408ec28750089c448476f38f599
Yoonji Park <ypark68@myseneca.ca>
2025-05-20 21:36:00 -0400

    lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Yoonji Park
+# Author ID: ypark68
+# Date Created: 2025/05/20
+
+import sys
+
+count = int(sys.argv[1])
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("blast off!")
+
